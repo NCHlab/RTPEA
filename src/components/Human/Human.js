c1c8076154f403dac241b49a95b00a5770414351
nchlab <bt17684@qmul.ac.uk>
2018-04-18 21:05:06 +0100

    hoverimage

--- src/components/Human/Human.js
+++ src/components/Human/Human.js
@@ -1,27 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Humanimg from './Human.png';
-import Humanimg2 from './Human_BW.png';
+import Humanimg2 from './Human_W.png';
 import Brain from './Brain.png';
 import './Human.css';
+import imagemap from './imagemap';
+import $ from 'jquery';
+import ImageMapper from 'react-image-mapper';
 
 
-const Human = () => {
-  return (
-  <div className="ma4 mt0">
-      <div class="human-img">
-        <div class="image_hover_bg"><img src={Humanimg2} className="pb6 imghov" alt="logo" height="650px" width="450px"/></div>
+let AREAS_MAP = {
+  name: "map1",
+  areas: [
+    { shape: "rect", coords: [80,6,123,57] /* Brain */ },
+    { shape: "circle", coords: [104,178,48] /* Lung */ },
+    { shape: "poly", coords: [57,242,67,231,78,228,91,231,106,235,123,234,134,238,133,244,124,252,117,257,104,259,97,262,87,266,83,272,76,271,68,269,62,272,58,277,55,256] /* Liver */ },
+    { shape: "poly", coords: [112,259,120,258,126,259,123,265,115,268,115,275,108,276,97,276,91,270,95,263] /* Pancreas */ },
+    { shape: "poly", coords: [59,279,58,299,58,319,68,328,76,324,79,331,92,330,94,336,99,342,105,343,107,334,108,325,116,326,130,325,138,318,142,304,141,293,141,274,143,264,135,259,124,269,117,275,106,279,91,277,76,273,67,271] /* Intestine */ },
+    { shape: "poly", coords: [87,340,86,348,93,349,113,351,117,344,113,338,102,345,93,338] /* Testes */ },
+    { shape: "circle", coords: [363,166,17] /* Heart */ },
+    { shape: "rect", coords: [311,184,398,202] /* Breast */ },
+    { shape: "poly", coords: [329,252,321,266,322,279,327,284,333,284,339,282,340,292,359,284,377,287,383,285,392,282,394,269,391,259,383,252,337,251] /* Kidney */ },
+    { shape: "poly", coords: [353,350,364,347,368,326,373,307,379,308,383,315,400,308,396,292,376,295,360,291,341,294,323,291,316,297,316,307,324,315,331,315,336,308,343,312,350,337] /* Uterus */ },
+  ]
+  };
+
+
+
+class Human extends Component{
+  constructor(props){
+    super(props)
+  }
+
+  componentDidMount = () => {
+
+  }
+
+
+  render (){
+    return (
+    <div className="ma4 mt0">
+        <div class="">
+          <ImageMapper src={Humanimg} map={AREAS_MAP} alt="" height={"650"} width={"450"}/>
+        </div>
+        {/* <img src={Humanimg} className="pb6 imghov" alt="logo" height="650px" width="450px"/>
+          <img id= "Brain" src={Brain} className="pb6 imghov" alt="logo"/> */}
+
+
+
+
+
       </div>
-      <img src={Humanimg} className="pb6 imghov" alt="logo" height="650px" width="450px"/>
-        <img id= "Brain" src={Brain} className="pb6 imghov" alt="logo"/>
-
-    </div>
-
-
 
   );
 }
+}
+
+{/* <div class="human-img">
+  <img src={Humanimg2} className="pb6 imghov" alt="logo" height="650px" width="450px"/>
+</div>
+<img src={Humanimg} className="pb6 imghov" alt="logo" height="650px" width="450px"/>
+  <img id= "Brain" src={Brain} className="pb6 imghov" alt="logo"/> */}
 
 export default Human;
+// const Human = () => {
+//   return (
+//   <div className="ma4 mt0">
+//     <img src={Humanimg2} className="pb6 imghov image_hover_bg" alt="logo" height="650px" width="450px"/>
+//     <img src={Humanimg} className="pb6 imghov human-img" alt="logo" height="650px" width="450px"/>
+//     <img id= "Brain" src={Brain} className="pb6 imghov" alt="logo"/>
+//
+//     </div>
+//
+//
+//
+//   );
+// }
+
+
 
 {/* <div style="width: 200px; height: 1000px; position: relative;">
    <img id="image1" style="position: relative;" src="..." alt="..." />
---
+++ src/components/Human/imagemap.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery'
+
+const imagemap = () => {
+  $(document).ready(function(e) {
+      $('img[usemap]').rwdImageMaps();
+  });
+}
+
+
+export default imagemap;
