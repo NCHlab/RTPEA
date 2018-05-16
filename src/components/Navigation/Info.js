d6a779c368d83699defcfc30cd12971965e41b0b
nchlab <bt17684@qmul.ac.uk>
2018-05-16 21:31:03 +0100

    update

--- src/components/Navigation/Info.js
+++ src/components/Navigation/Info.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import Flowchart from "../Info/FlowChart_Msc.png";
 
 const Info = () => (
-  <div>
-    <h2>Info page</h2>
-  </div>
+	<div>
+
+		<div className="container">
+      <h2>RTPEA Flowchart</h2>
+			<img src={Flowchart} alt="FlowChart" />
+		</div>
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+	</div>
 );
 
 export default Info;
