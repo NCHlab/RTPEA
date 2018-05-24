ee17baaf52ef96ffea8b7de4556e5a8929e1da27
nchlab <bt17684@qmul.ac.uk>
2018-05-24 20:49:06 +0100

    update

--- src/components/Navigation/Browse.js
+++ src/components/Navigation/Browse.js
@@ -50,6 +50,7 @@ class Table extends Component{
   // accessor: "0.sample[0].1[0].file_name"
 
   const mynum = 20
+  const blank_data = "-"
 
   const main_columns = [{
     Header: 'Data by Study',
@@ -71,8 +72,51 @@ class Table extends Component{
   }, {
     Header: 'Tissue',
     accessor: 'sample[0].1[0].tissue_type'
+  // }, {
+  //   Header: 'ORF1p',
+  //   accessor: "-"
+  // }, {
+  //   Header: 'ORF2p',
+  //   accessor: "-"
+  // }, {
+  //   Header: 'ORF0',
+  //   accessor: "-"
+  // }, {
+  //   Header: 'HERV-K',
+  //   accessor: "-"
+  // }, {
+  //   Header: 'HERV-A',
+  //   accessor: "-"
+  // }, {
+  //   Header: 'HERV-V',
+  //   accessor: "-"
   }]
   }]
+  var mylist = [{"1":2,"2":2,"3":3}]
+
+
+//   Header: 'Sample Number',
+//   id: "Sample_num",
+//   accessor: data => {var mylist=[]
+//                       for (var i in data.sample[0]){
+//                       mylist.push([i])
+//                       console.log([i])
+//
+//                     }
+//
+//                       console.log(mylist)
+//                       return mylist
+//                     }
+//                     // console.log(data.sample.length);
+//                     // data.sample[0][1][0].replicate
+// }, {
+
+// You would need to materialize 2 separate rows
+// You would have to create an additional row in your array for each item
+// You cannot materialize additional rows from within a column accessor
+// This is more of a javascript question
+// You would just need to make a new item in the array you pass in
+// for each additional row you want to display
 
   const sec_columns = [{
     Header: '',
@@ -82,14 +126,80 @@ class Table extends Component{
     accessor: '-'
   }, {
     Header: 'Sample Number',
-    accessor: '-'
-  }, {
-    Header: 'ORFP2',
-    accessor: 'sample[0].1[0].ORF2p.confidence'
+    id: "Sample_num",
+      accessor: data => {var mylist=[]
+                          for (var i in data.sample[0]){
+                          mylist.push(i)
+                          // console.log(i)
+
+                        }
+
+                          // console.log(mylist)
+                          return mylist
+                        }
+                        // console.log(data.sample.length);
+                        // data.sample[0][1][0].replicate
+    }, {
+    Header: 'Disease',
+    accessor: 'disease'
   }, {
     Header: 'Tissue',
-    id: "num2",
-    accessor: (d) => mynum
+    id: "tiss_data",
+    accessor: data =>{
+      // console.log(data.sample[0][1][0].tissue_type)
+                        return data.sample[0][1][0].tissue_type
+                      }
+    // accessor: 'sample[0].1[0].tissue_type'
+  }, {
+    Header: 'ORF1p',
+    accessor: "sample[0].1[0].ORF1p.confidence"
+  }, {
+    Header: 'ORF2p',
+    id: "ORF_data",
+    accessor: "sample[0].1[0].ORF2p.confidence",
+    Cell: row => (
+          <div
+            style={{
+              width: '100%',
+              height: '100%',
+              backgroundColor: '#dadada',
+              borderRadius: '2px'
+            }}
+          >
+            {/* {row.value} */}
+            <div
+              style={{
+                width: `${row.value}%`,
+                height: '100%',
+                backgroundColor: row.value > 80 ? '#85cc00'
+                  : row.value > 20 ? '#ffbf00'
+                  : '#ff2e00',
+                borderRadius: '2px',
+                transition: 'all .2s ease-out'
+              }}
+            >
+              {row.value}
+            </div>
+
+
+
+
+        </div>
+        )
+    }
+    // "sample[0].1[0].ORF1p.confidence" && "sample[0].1[0].ORF2p.confidence"
+  , {
+    Header: 'ORF0',
+    accessor: "sample[0].1[0].ORF0.confidence"
+  }, {
+    Header: 'HERV-K',
+    accessor: "sample[0].1[0].HERV-K.confidence"
+  }, {
+    Header: 'HERV-A',
+    accessor: "sample[0].1[0].HERV-A.confidence"
+  }, {
+    Header: 'HERV-V',
+    accessor: "sample[0].1[0].HERV-V.confidence"
   }]
 
 
@@ -114,7 +224,8 @@ class Table extends Component{
               })
           };
         }} */}
-
+        {/* pivotBy={["Sample_num", "ORF_data"]} */}
+{/* pivotBy={["disease"]} */}
             <ReactTable
               loading={this.state.table_loading}
               data={this.state.data2}
@@ -125,12 +236,14 @@ class Table extends Component{
               showPaginationTop={true}
               className="-striped -highlight"
               pageSizeOptions={[5, 10, 20, 25, 50, 100, 200]}
+
               SubComponent={row => {
                 return (
                   <div style={{ border: "4px", borderStyle: "dotted solid solid solid", borderColor: "rgb(0, 83, 140)" }}>
                     {console.log(row.original)}
                     <ReactTable
                       data={[row.original]}
+
                       columns={sec_columns}
                       defaultPageSize={3}
                       showPagination={false}
@@ -139,7 +252,7 @@ class Table extends Component{
                     );
                   }}
                 />
-
+  {/* resolveData={data => data.map(row => row)} */}
 {/* data={row.row._original[0]} */}
 
 
