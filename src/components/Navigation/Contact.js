260464efab9384378a9842b0a538a81129245ec4
nchlab <bt17684@qmul.ac.uk>
2018-05-26 03:12:12 +0100

    CSS + web tabs

--- src/components/Navigation/Contact.js
+++ src/components/Navigation/Contact.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
+import github_logo from '../ContactUs/github.png';
+import linkedin_logo from '../ContactUs/linkedin.png';
 
 
 const Project = () => (
   <div>
-    <h2>Website Created by: Nayam Chowdhury
-    <br />
-  with help from Nazrath Nawaz</h2>
+
+    <div className="contact_h1">
+      <h1>Website Created by: Nayam Chowdhury</h1>
+    </div>
+    <div className="contact_h1">
+      <h1>with help from Nazrath Nawaz</h1>
+    </div>
+	<div className="contact_container">
+
+    <div className="contact_img">
+      <a href="http://github.com/nchlab">
+        <img className="contact_img" src={github_logo}/>
+      </a>
+      <img className="contact_img" src={linkedin_logo}/>
+
+    </div>
+	</div>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
   </div>
 );
 
--- src/components/Navigation/Info.js
+++ src/components/Navigation/Info.js
@@ -1,12 +1,81 @@
 import React, { Component } from "react";
 import Flowchart from "../Info/FlowChart_Msc.png";
+import ImageMapper from 'react-image-mapper';
 
-const Info = () => (
+let Soft_map = {
+	name: "software architecture map",
+	areas: [
+		{ shape: "rect", coords: [1,24,363,600],  /* View */ },
+		{ shape: "rect", coords: [364,24,545,600],  /* Controller */ },
+		{ shape: "rect", coords: [546,24,908,600],  /* Model */ },
+
+	]
+	};
+	//[Left,Top,Right,Bottom]
+
+	// { shape: "rect", coords: [1,21,320,440],  /* View */ },
+	// { shape: "rect", coords: [322,21,480,440],  /* Controller */ },
+	// { shape: "rect", coords: [481,20,800,440],  /* Model */ },
+	class Info extends Component{
+	  constructor(props){
+	    super(props)
+	    this.state = {
+	      loading: "false",
+	      highlight: false,
+	      index: "",
+	    };
+
+	  }
+
+
+	  componentDidMount = () => {
+	  }
+
+
+
+	  MouseEnter = (area, index, event) => {
+	    this.setState({loading: "true",
+	    index: index});
+	    // console.log(index);
+
+	  }
+
+		MouseLeave = () => {
+	    this.setState({loading: "false"});
+	  }
+
+		ImageArray = (index) => {
+	    if (index === 0){
+	      return (<div>
+					This consists of the View made by using ReactJs
+					<br/>
+					-
+				</div>)
+	    } else if (index === 1){
+	      return(
+					<div>
+						This consists of the Controller which is where the API server is located. All data is processed through the controller.
+					</div>)
+	    } else if (index === 2){
+	      return(
+					<div>
+						This consists of the Model where the data is handled and stored.
+						<br/>
+						-
+					</div>)
+	    }
+	  }
+render (){
+
+	return (
 	<div>
+
 
 		<div className="container">
       <h2>RTPEA Flowchart</h2>
-			<img src={Flowchart} alt="FlowChart" />
+			<h2>{this.ImageArray(this.state.index)}</h2>
+			<ImageMapper src={Flowchart}  map={Soft_map} fillColor="rgba(0, 0, 0, 0.1)" className="pb6" alt="" height={"500"} width={"908"} onMouseEnter={this.MouseEnter} />
+			{/* <img src={Flowchart} alt="FlowChart" height="600px" width="1090px" /> */}
 		</div>
 		<br />
 		<br />
@@ -16,5 +85,7 @@ const Info = () => (
 		<br />
 	</div>
 );
+}
+}
 
 export default Info;
