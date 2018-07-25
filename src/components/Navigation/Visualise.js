1e8413fcbb311c08bd5b4505b73f71d941d6e9c8
nchlab <bt17684@qmul.ac.uk>
2018-07-25 14:21:17 +0100

    Sorts the Filter list by Alphabetical order

--- src/components/Navigation/Visualise.js
+++ src/components/Navigation/Visualise.js
@@ -20,6 +20,8 @@ class Visualisation extends Component {
 			data_info: "",
 		};
 
+		this.SortVariantList = this.SortVariantList.bind(this);
+		this.WaitingTime = this.WaitingTime.bind(this);
 	}
 
 
@@ -84,7 +86,49 @@ class Visualisation extends Component {
 			// }))
 				//
 				// instance.selectFeature('variant', 108, 108, 'K')
-    }
+
+}
+
+		SortVariantList = () => {
+		  var list, i, switching, b, shouldSwitch;
+		  list = document.getElementById("orderthelist");
+		  switching = true;
+		  /*Make a loop that will continue until
+		  no switching has been done:*/
+		  while (switching) {
+			//start by saying: no switching is done:
+			switching = false;
+			b = list.getElementsByTagName("LI");
+			//Loop through all list-items:
+			for (i = 1; i < (b.length - 1); i++) {
+			  //start by saying there should be no switching:
+			  shouldSwitch = false;
+			  /*check if the next item should
+			  switch place with the current item:*/
+			  if (b[i].innerText.toLowerCase() > b[i + 1].innerText.toLowerCase()) {
+				/*if next item is alphabetically
+				lower than current item, mark as a switch
+				and break the loop:*/
+				shouldSwitch = true;
+				break;
+			  }
+			}
+			if (shouldSwitch) {
+			  /*If a switch has been marked, make the switch
+			  and mark the switch as done:*/
+			  b[i].parentNode.insertBefore(b[i + 1], b[i]);
+			  switching = true;
+			}
+		  }
+		}
+
+	WaitingTime= () => {
+  setTimeout(() => this.SortVariantList(), 1000);
+}
+
+
+
+
 
 		// componentDidUpdate = () => {
 		// 	instance.selectFeature("variant", 125, 128, 'VAS');
@@ -101,6 +145,7 @@ class Visualisation extends Component {
 			<div>
 
 				{console.log(this.state.data_info)}
+
 			<div className="background-body-vis">
 				<NavVis/>
 				<div className="text-center">
@@ -192,7 +237,8 @@ class Visualisation extends Component {
 						{/* ProtVista Loads Here */}
 					</div>
 				</div>
-
+				{/* <button onClick={() => this.sortList()}>CLICK </button> */}
+				{window.onload = this.WaitingTime()}
 				{console.log(this.props.match.params.id)}
 
 
