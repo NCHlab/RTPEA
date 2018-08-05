d39a7d4073113358090e529dc25107d71a21b67d
nchlab <bt17684@qmul.ac.uk>
2018-08-05 01:14:29 +0100

    ContactPage

--- src/components/Navigation/Contact.js
+++ src/components/Navigation/Contact.js
@@ -4,6 +4,10 @@ import linkedin_logo from "../ContactUs/linkedin.png";
 import nchlab from "../ContactUs/github_nchlab.png";
 import nazrath10r from "../ContactUs/github_nazrath10r.png";
 import qmul_logo from "../ContactUs/QMUL_SBCS.png";
+import nayam from "../ContactUs/nayam.png";
+import nazrath from "../ContactUs/nazrath.png";
+import conrad from "../ContactUs/conrad.png";
+import miguel from "../ContactUs/miguel.png";
 
 const Project = () => (
   <div>
@@ -27,13 +31,131 @@ const Project = () => (
     </div>
 
 
+    <div className="container">
+      <hr />
+    </div>
 
 
 
+    <div className="container" style={{marginLeft:"20%"}}>
+
+      <img
+        className="contact_img"
+        src={nayam}
+        width="300px"
+        height="300px"
+      />
+
+      <div className="description_style contact_img" style={{top:"400px"}}>
+        <a href="https://github.com/nchlab">Github - NCHLAB</a>
+      </div>
+      <div className="description_style contact_img" style={{top:"450px"}}> Linkedin</div>
+      <div className="description_style contact_img" style={{top:"500px"}}> Contact: m.a.n.chowdhury@se17.qmul.ac.uk</div>
+
+
+      <p>
+        <br/>
+      <button className="btn btn-outline-primary">Lead Dev</button> &nbsp;
+      <button className="btn btn-outline-primary">Website Creator</button> &nbsp;
+      <button className="btn btn-outline-primary">JavaScript</button> &nbsp;
+      <button className="btn btn-outline-primary">ReactJS</button> &nbsp;
+      <button className="btn btn-outline-primary">MongoDB</button> &nbsp;
+      <button className="btn btn-outline-primary">HTML</button> &nbsp;
+      <button className="btn btn-outline-primary">CSS</button> &nbsp;
+      <button className="btn btn-outline-primary">????</button> &nbsp;
+
+    </p>
+    <p>
+      <span style={{paddingLeft:"250px"}}>
+      <button className="btn btn-outline-primary">Msc BioInformatics</button> &nbsp;
+      <button className="btn btn-outline-primary">????</button> &nbsp;
+      <button className="btn btn-outline-primary">????</button> &nbsp;
+    </span>
+
+    </p>
+    </div>
 
     <div className="container">
       <hr />
     </div>
+
+    <div className="container" style={{marginLeft:"20%"}}>
+      <div class="row">
+    <div class="col-sm">
+      <img
+        style={{marginLeft:"20%"}}
+        className="contact_img"
+        src={nazrath}
+        width="200px"
+        height="200px"
+      />
+
+      <div className="contact_img description_style2" style={{top:"850px"}}>
+        <a href="https://github.com/nazrath10r">Github - nazrath10r</a>
+      </div>
+      <div className="contact_img description_style2" style={{top:"900px"}}> Linkedin</div>
+      <div className="contact_img description_style2" style={{top:"950px"}}> m.n.mohamednawaz@qmul.ac.uk</div>
+
+      <p>
+        <br/>
+      <button className="btn btn-outline-primary">Co-Dev Supervisor</button> &nbsp;
+      <button className="btn btn-outline-primary">RetroMiner Expert</button> &nbsp;
+      <br/>
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      <button className="btn btn-outline-primary">Data Generation</button> &nbsp;
+    </p>
+</div>
+<div class="col-sm">
+      <img
+        style={{marginLeft:"20%"}}
+        className="contact_img"
+        src={conrad}
+        width="200px"
+        height="200px"
+      />
+
+      <div className="contact_img description_style2" style={{top:"850px"}}>
+        <a href="https://github.com/">Github - Conrad Bessant</a>
+      </div>
+      <div className="contact_img description_style2" style={{top:"900px"}}> Linkedin</div>
+      <div className="contact_img description_style2" style={{top:"950px"}}> c.bessant@qmul.ac.uk</div>
+
+      <p>
+        <br/>
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      <button className="btn btn-outline-primary">MSc Supervisor</button> &nbsp;
+    </p>
+    </div>
+
+    <div class="col-sm">
+      <img
+        style={{marginLeft:"20%"}}
+        className="contact_img"
+        src={miguel}
+        width="200px"
+        height="200px"
+      />
+
+      <div className="contact_img description_style2" style={{top:"850px"}}>
+        <a href="https://github.com/">Github - Miguel</a>
+      </div>
+      <div className="contact_img description_style2" style={{top:"900px"}}> Linkedin</div>
+      <div className="contact_img description_style2" style={{top:"950px"}}> m.branco@qmul.ac.uk</div>
+
+      <p>
+        <br/>
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      <button className="btn btn-outline-primary">Biological Insights</button> &nbsp;
+    </p>
+    </div>
+  </div>
+</div>
+
+    <div className="container">
+      <hr />
+    </div>
+
+
 
     <div className="contact_container">
       <div className="contact_img">
