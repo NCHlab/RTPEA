56cb94ed047ef1839aa4d6d20e2bcdb05de8a34b
nchlab <bt17684@qmul.ac.uk>
2018-08-16 23:49:28 +0100

    Code examples

--- src/components/Navigation/Download.js
+++ src/components/Navigation/Download.js
@@ -6,6 +6,9 @@ import Switch from "react-toggle-switch";
 import Popup from "reactjs-popup";
 import "react-toggle-switch/dist/css/switch.min.css";
 import NavDownload from "./NavDownload.js";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import Highlight from "react-highlight";
+import "../Highlight/styles/vs2015.css";
 // ../../../node_modules/
 
 class Download extends Component {
@@ -27,14 +30,13 @@ class Download extends Component {
       text_colour_err: "#af0603",
       button_msg: "Darkify",
       switched: true,
-      name_of_file:"",
+      name_of_file: ""
     };
     // This binding is necessary to make `this` work in the callback
 
     this.button_click = this.button_click.bind(this);
     this.download_table = this.download_table.bind(this);
     this.download_l1seq = this.download_l1seq.bind(this);
-
   }
   componentDidMount = () => {
     this.setState({ isLoading: false });
@@ -58,8 +60,6 @@ class Download extends Component {
     // let url = "http://rtpea.com/api/" + url_id;
     return url;
   };
-
-
 
   //Upon this function call, SearchUrl function is also called
   // the returning url is then converted to JSON
@@ -86,23 +86,23 @@ class Download extends Component {
       });
   };
 
-  download_table = () =>{
-    this.setState({name_of_file: "Table"})
-    fetch(this.props.urlSource+"/table")
-    .then(response => response.json())
-    .then(data => {
-       this.saveAs(JSON.stringify(data, null, 2))
-  })
-}
+  download_table = () => {
+    this.setState({ name_of_file: "Table" });
+    fetch(this.props.urlSource + "/table")
+      .then(response => response.json())
+      .then(data => {
+        this.saveAs(JSON.stringify(data, null, 2));
+      });
+  };
 
-download_l1seq = () =>{
-  this.setState({name_of_file: "Line-1_Sequences"})
-  fetch(this.props.urlSource+"/sequence/all")
-  .then(response => response.json())
-  .then(data => {
-     this.saveAs(JSON.stringify(data, null, 2))
-})
-}
+  download_l1seq = () => {
+    this.setState({ name_of_file: "Line-1_Sequences" });
+    fetch(this.props.urlSource + "/sequence/all")
+      .then(response => response.json())
+      .then(data => {
+        this.saveAs(JSON.stringify(data, null, 2));
+      });
+  };
 
   // This function allows the user to save the data as a file.
   saveAs = (
@@ -125,10 +125,7 @@ download_l1seq = () =>{
     }
   };
 
-
-
   render() {
-
     return (
       <div>
         <div className="line-seperator" />
@@ -139,14 +136,11 @@ download_l1seq = () =>{
           className="ma4 mt0 background-body4-noalign container col-md-9"
           style={{ backgroundColor: this.state.background_colour }}
         >
-
           <div
             className="background-body4"
             style={{ backgroundColor: this.state.data_background_colour }}
           >
             <div class="container">
-
-
               <table id="download_table" className="table-border">
                 <tr>
                   <th>Download</th>
@@ -165,7 +159,8 @@ download_l1seq = () =>{
                 </tr>
                 <tr>
                   <td>
-                    <button className="btn btn-info"
+                    <button
+                      className="btn btn-info"
                       onClick={e => this.download_l1seq()}
                     >
                       Download all L1 Prot Sequences
@@ -192,15 +187,100 @@ download_l1seq = () =>{
                 </tr>
                 <tr>
                   <td>
-                    <button className="btn btn-info"
-                      onClick={e => {window.location = "../api"}}
-                          >
+                    <button
+                      className="btn btn-info"
+                      onClick={e => {
+                        window.location = "../api";
+                      }}
+                    >
                       Go to API
                     </button>
                   </td>
-                  <td>For Manual and Programmatic Access to the Application Programming Interface</td>
+                  <td>
+                    For Manual and Programmatic Access to the Application
+                    Programming Interface
+                  </td>
                 </tr>
               </table>
+
+              <Tabs>
+                <TabList>
+                  <Tab>Python</Tab>
+                  <Tab>javaScript</Tab>
+                  <Tab>R</Tab>
+                </TabList>
+
+                <TabPanel>
+                  <Highlight language="python">
+                    {`
+import json
+import urllib2
+import json
+List_of_PXD = ["PXD002211","PXD002212","PXD004682","PXD003407","PXD003409","PXD003552","PXD005733","PXD004818"]
+
+for i in List_of_PXD:
+    url = "https://api.rtpea.com/api/" + i '# open the url and the screen name'
+    data = json.load(urllib2.urlopen(url)) '# Python objects dumped into JSON string'
+
+    with open(i+".json", "w") as outfile: '# Saves the file to JSON as PXD.json'
+        json.dump(data, outfile)
+`}
+                  </Highlight>
+                </TabPanel>
+                <TabPanel>
+                  <Highlight language="javascript">
+                    {`
+var fetchUrl = require("fetch").fetchUrl;
+const fs = require("fs"); //file system library
+
+List_of_PXD = ["PXD002211","PXD002212","PXD004682","PXD003407","PXD003409","PXD003552","PXD005733","PXD004818"]
+
+// Fetches data from URL and parses into JSON, before bring turned to string to convert to readable form
+for (var i = 0; i < List_of_PXD.length; i++){
+  fetchUrl("http://localhost:3001/api/"+List_of_PXD[i],  function(error, meta, body){
+      parsed_data = JSON.parse(body)
+      json_data = JSON.stringify(parsed_data)
+      // Writes to js_json folder with PXD name
+      fs.writeFile("./js_json/"+parsed_data[0].PXD+".json", body, (err) => {
+          if (err) {
+              console.error(err);
+              return;
+          };
+          console.log("File has been created");
+      })
+      });
+}
+
+                    `}
+                  </Highlight>
+                </TabPanel>
+                <TabPanel>
+                  <Highlight language="R">
+                    {`
+library(jsonlite)
+
+setwd("D:/UserDirectory/")
+
+write.json <- function(x, file = "", ...) {
+  x.json <- toJSON(x, pretty = TRUE, na='string', auto_unbox = TRUE)
+  write(x.json, file=file)
+}
+
+List_of_PXD = list("PXD002211","PXD002212","PXD004682","PXD003407","PXD003409","PXD003552","PXD005733","PXD004818")
+
+
+for (i in List_of_PXD){
+  '# url that hosts data'
+  url <- paste('https://api.rtpea.com/api/',i,sep="")
+  '# read url and convert to data.frame'
+  document <- fromJSON(txt=url)
+  '# Writes to file in folder r_result as PXD.json'
+  write.json(document, file = paste("./r_result/", i,".jSON", sep=""))
+}
+                    `}
+                  </Highlight>
+                </TabPanel>
+              </Tabs>
             </div>
           </div>
 
