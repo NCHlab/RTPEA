ea0e297c59091f3ac03f04069e6e0c917e829f8e
nchlab <bt17684@qmul.ac.uk>
2019-06-20 21:16:12 +0100

    Replaced click/hover button, now uses user clicks + added active svg css

--- src/components/Human/Human.js
+++ src/components/Human/Human.js
@@ -1,18 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-// import Humanimg from "./Human1.png";
-// import Svg1 from "./human_non_colourised.svg";
-// import Svg2 from "./human_colourised.svg";
+
 import Svg3 from "./human_colourised_lung_grouped.svg";
-// import Humanimg2 from "./Human_W.png";
-// import Human_lung from "./Human_lung.png";
 import L1Chart from "../Images/L1-Identifications.png";
-// import Brain from "./Brain.png";
-// import Plot1 from "./plot1.png";
-// import Plot3 from "./plot3.png";
-// import Plot4 from "./plot4.png";
-// import Plot5 from "./plot5.png";
-// import Plot6 from "./plot6.png";
+
 import brain_data from "../Files/brain.json";
 import lung_data from "../Files/lung.json";
 import liver_data from "../Files/liver.json";
@@ -30,11 +21,19 @@ import "./Human.css";
 import $ from "jquery";
 import SVG from "react-inlinesvg";
 import Loader from "react-loader";
-import { LineChart, Line, BarChart, Bar, YAxis, XAxis,CartesianGrid, Tooltip, Legend, Cell } from 'recharts';
-import Browse from '../Navigation/Browse';
-
-
-
+import {
+  LineChart,
+  Line,
+  BarChart,
+  Bar,
+  YAxis,
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  Cell
+} from "recharts";
+import Browse from "../Navigation/Browse";
 
 class Human extends Component {
   constructor(props) {
@@ -47,191 +46,196 @@ class Human extends Component {
       svgType: undefined,
       x: 0,
       y: 0,
-      chartWidth:800,
+      chartWidth: 800,
       chartHeight: 600,
       yWidth: 120,
-      whiteFill:{ fill: 'white', fontSize: 14},
-      brain_data:brain_data,
-      lung_data:lung_data,
-      liver_data:liver_data,
-      uterus_data:uterus_data,
-      breast_data:breast_data,
-      heart_data:heart_data,
-      intestine_data:intestine_data,
-      kidney_data:kidney_data,
-      pancreas_data:pancreas_data,
-      testes_data:testes_data,
+      whiteFill: { fill: "white", fontSize: 14 },
+      current_active_tissue: "",
+      brain_data: brain_data,
+      lung_data: lung_data,
+      liver_data: liver_data,
+      uterus_data: uterus_data,
+      breast_data: breast_data,
+      heart_data: heart_data,
+      intestine_data: intestine_data,
+      kidney_data: kidney_data,
+      pancreas_data: pancreas_data,
+      testes_data: testes_data,
       all_other_tissue_data: all_other_tissue_data,
       all_data: all_data,
       testes_data: testes_data,
-      clicktochange:"Hover over tissue to display graph",
-      hovertochange:"Click on tissue to display graph",
-
-
-
+      clicktochange: "Hover over tissue to display graph",
+      hovertochange: "Click on tissue to display graph"
     };
 
     // this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
     this.detectSvg = this.detectSvg.bind(this);
     this.onClickHandler = this.onClickHandler.bind(this);
+    this.onMouseOverHandler = this.onMouseOverHandler.bind(this);
+    this.makeActive = this.makeActive.bind(this)
   }
 
   componentDidMount = () => {};
 
   detectSvg = svgType => {
-    var data_type = []
-    var urlstate = ""
+    var data_type = [];
+    var urlstate = "";
     if (svgType === "liver") {
-      data_type = this.state.liver_data
-    } else if (svgType === "lung"){
-      data_type = this.state.lung_data
-    }else if (svgType === "uterus"){
-      data_type = this.state.uterus_data
-    }else if (svgType === "testes"){
-      data_type = this.state.testes_data
-    }else if (svgType === "pancreas"){
-      data_type = this.state.pancreas_data
-    }else if (svgType === "kidney"){
-      data_type = this.state.kidney_data
-    }else if (svgType === "intestine"){
-      data_type = this.state.intestine_data
-    }else if (svgType === "heart"){
-      data_type = this.state.heart_data
-    }else if (svgType === "breasts"){
-      data_type = this.state.breast_data
-      urlstate = "breast"
-    }else if (svgType === "brain"){
-      data_type = this.state.brain_data
-    }else if (svgType === undefined){
-      data_type = this.state.all_data
+      data_type = this.state.liver_data;
+    } else if (svgType === "lung") {
+      data_type = this.state.lung_data;
+    } else if (svgType === "uterus") {
+      data_type = this.state.uterus_data;
+    } else if (svgType === "testes") {
+      data_type = this.state.testes_data;
+    } else if (svgType === "pancreas") {
+      data_type = this.state.pancreas_data;
+    } else if (svgType === "kidney") {
+      data_type = this.state.kidney_data;
+    } else if (svgType === "intestine") {
+      data_type = this.state.intestine_data;
+    } else if (svgType === "heart") {
+      data_type = this.state.heart_data;
+    } else if (svgType === "breasts") {
+      data_type = this.state.breast_data;
+      urlstate = "breast";
+    } else if (svgType === "brain") {
+      data_type = this.state.brain_data;
+    } else if (svgType === undefined) {
+      data_type = this.state.all_data;
     } else {
-        data_type= this.state.all_other_tissue_data
-      }
-    var data_color=["#53b4d8","#c60000","#c60000","#3ec629","#3ec629","#185bce","#185bce","#185bce","#cc7f28"]
-    // <Bar type="monotone" dataKey="Number" barSize={25} fill={["#53b4d8"]} onMouseOver={{cursor:'pointer'}}/>
+      data_type = this.state.all_other_tissue_data;
+    }
+    var data_color = [
+      "#53b4d8",
+      "#c60000",
+      "#c60000",
+      "#3ec629",
+      "#3ec629",
+      "#185bce",
+      "#185bce",
+      "#185bce",
+      "#cc7f28"
+    ];
+
     return (
-    // style={{borderLeft:"2px solid #ccc",borderRight:"2px solid #ccc",borderBottom:"2px solid #ccc",borderColor:"#bababa"}}>
+
       <div>
-        <div style={{textAlign:"center"}}><h3>Summary Metric: {this.state.svgType == "whole_human_img" ? "Other Tissues" : this.state.svgType == undefined ? "All Tissues" : this.state.svgType.slice(0,1).toUpperCase()+this.state.svgType.slice(1)}</h3></div>
-        <BarChart  layout="vertical"  width={this.state.chartWidth} height={this.state.chartHeight} data={data_type} onClick={() => window.location.assign(this.props.urlSource2+"/browse/"+urlstate)}>
-          <XAxis type="number" tick={this.state.whiteFill} />
-          <YAxis dataKey="name" type="category" width={this.state.yWidth} tick={this.state.whiteFill} />
-          <Tooltip cursor={{cursor:'pointer'}} wrapperStyle={{color:"black"}} itemStyle={{color:"#001fbf"}}/>
-          <Bar dataKey="Number" type="monotone" barSize={25} onMouseOver={{cursor:'pointer'}}>
-           {
-             data_color.map((entry, index) => (
-               <Cell key={`cell-${index}`} fill={data_color[index]} />
-             ))
-           }
-         </Bar>
+        <div style={{ textAlign: "center" }}>
+          <h3>
+            Summary Metric:{" "}
+            {this.state.svgType == "whole_human_img" ? "Other Tissues" : this.state.svgType == undefined
+              ? "All Tissues" : this.state.svgType.slice(0, 1).toUpperCase() + this.state.svgType.slice(1)}
+          </h3>
+        </div>
+        <BarChart
+          layout="vertical"
+          width={this.state.chartWidth}
+          height={this.state.chartHeight}
+          data={data_type}
+          onClick={() => window.location.assign(this.props.urlSource2 + "/browse/" + urlstate)}>
+          <XAxis type="number" tick={this.state.whiteFill}/>
+          <YAxis dataKey="name" type="category" width={this.state.yWidth} tick={this.state.whiteFill}/>
+          <Tooltip cursor={{ cursor: "pointer" }} wrapperStyle={{ color: "black" }} itemStyle={{ color: "#001fbf" }}/>
+          <Bar dataKey="Number" type="monotone" barSize={25} onMouseOver={{ cursor: "pointer" }}>
+            {data_color.map((entry, index) => (<Cell key={`cell-${index}`} fill={data_color[index]} />))}</Bar>
         </BarChart>
       </div>
-    )
+    );
   };
 
-  handleChange= () => {
-   this.setState({
-     checked: !this.state.checked
-   })
- }
+  // handleChange = () => {
+  //   this.setState({
+  //     checked: !this.state.checked
+  //   });
+  // };
+
+
+  makeActive = e =>  {
+    if (this.state.current_active_tissue !== ""){
+    // Removing active colour for the current active tissue
+      var ClassNameChange = document.getElementsByClassName(this.state.current_active_tissue)[0]
+      if (ClassNameChange !== undefined && ClassNameChange !== 'whole_human_img'){
+        ClassNameChange.attributes.class.value = ClassNameChange.attributes.class.value.replace(/\organ-svg-clicked\b/g, "")
+        }
+    }
+
+    var target_tissue = e.target.classList[1]
+    this.setState({current_active_tissue:target_tissue})
+    if (target_tissue !== undefined && target_tissue !== 'whole_human_img'){
+    document.getElementsByClassName(target_tissue)[0].classList += ' organ-svg-clicked'
+  }
+
+}
 
   onClickHandler = e => {
-    this.setState({ svgType: e.target.classList[1]});
-    // this.detectSvg()
-    // console.log("I'm clicked");
-    // console.log(result_svg);
-    // console.log(e.target.classList[1]);
-    // console.log(result_svg.className.animVal);
-    // console.log(result_svg.attributes[0].value);
+    this.setState({ svgType: e.target.classList[1] });
+    if (this.state.checked !== true) {
+      this.setState({ checked: true });
+    }
+    this.makeActive(e)
+  };
+
+  onMouseOverHandler = e => {
+    this.setState({ svgType: e.target.classList[1] });
   };
 
   svgWrapper_onClick = ({ dangerouslySetInnerHTML, className }) => {
-      return (
-        <span
-          onClick={e => this.onClickHandler(e)}
-          dangerouslySetInnerHTML={dangerouslySetInnerHTML}
-          className={className}
-        />
-      );
-  };
-
-  svgWrapper_mouseOver = ({ dangerouslySetInnerHTML, className }) => {
     return (
       <span
-        onMouseOver={e => this.onClickHandler(e)}
+        onClick={e => this.onClickHandler(e)}
         dangerouslySetInnerHTML={dangerouslySetInnerHTML}
         className={className}
       />
     );
   };
 
-check_event = (e) =>{
-  console.log(e)
-}
-
+  svgWrapper_mouseOver = ({ dangerouslySetInnerHTML, className }) => {
+    return (
+      <span
+        onMouseOver={e => this.onMouseOverHandler(e)}
+        onClick={e => this.onClickHandler(e)}
+        dangerouslySetInnerHTML={dangerouslySetInnerHTML}
+        className={className}
+      />
+    );
+  };
 
   render() {
-
-    const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-];
-
     return (
       <div>
-        {/* {console.log(this.props)} */}
-        {/* style={{cursor:'pointer'}} */}
-        {/* <input checked data-toggle="toggle" type="checkbox"/> */}
-      <sup>Currently On:</sup> &emsp;
-
-      <button style={{float:"", display: "inline"}} className="btn btn-outline-warning" onClick={() => {this.handleChange()}}> {this.state.checked ? "Click Mode" : "Hover Mode"} </button>
-
-      {/* <input   type="button" data-on="Enabled" data-off="Disabled" checked={ this.state.checked } onClick={() => {this.handleChange()
-      console.log(this.state.checked)}}/> */}
-      <br/>
-      <br/>
-
-      <label style={{border:"2px" , borderStyle: "none none solid none" , borderColor:"#ffffff"}}><b>{this.state.checked ? this.state.hovertochange : this.state.clicktochange}</b></label>
-        {/* <label style={{border:"2px" , borderStyle: "none none solid none" , borderColor:"#ffffff"}}><b>Enable to Browse by clicking tissue</b></label> */}
-
-      <br/>
-      <br/>
-      <div className="ma4 mt0">
-
-        <div className="container c1">
-          <div className="human-img">
-            <div className="go-left">
-              <div id="human-svg-1">
-                <SVG
-                  src={Svg3}
-                  uniquifyIDs={true}
-                  wrapper={this.state.checked ? this.svgWrapper_onClick : this.svgWrapper_mouseOver}
-                />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className="ma4 mt0">
+          <div className="container c1">
+            <div className="human-img">
+              <div className="go-left">
+                <div id="human-svg-1">
+                  <SVG
+                    src={Svg3}
+                    uniquifyIDs={false}
+                    wrapper={
+                      this.state.checked
+                        ? this.svgWrapper_onClick
+                        : this.svgWrapper_mouseOver}/>
+                </div>
               </div>
             </div>
-          </div>
-{/* style={{background:"#d8ecff",Color:"white"} */}
 
 
-          <div className="inline-img"  >
-            <br/>
-            <br/>
-            {this.detectSvg(this.state.svgType)}
-            {/* {console.log(this.state.svgType)} */}
+            <div className="inline-img">
+              <br />
+              <br />
+              {this.detectSvg(this.state.svgType)}
+            </div>
           </div>
+          <br />
+          <br />
         </div>
-        <br />
-        <br />
       </div>
-      {/* <BarChart /> */}
-
-    </div>
     );
   }
 }
